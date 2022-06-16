7cbe7ac862b7d999bc0d87cc148e0d4f5f5650ad
kikitsa <kiki_paniskaki@hotmail.com>
2022-06-16 15:15:05 +0300

    Injecting the user

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
 import TheHeader from "@/components/TheHeader.vue";
 import EntryEditor from "./components/EntryEditor.vue";
 import EntryCard from "@/components/EntryCard.vue";
-import { reactive } from "vue";
+import { reactive, provide, inject } from "vue";
 import type User from "./types/User";
 import type Entry from "./types/Entry";
+import { userInjectionKey } from "./injectionKeys";
 
 const entries: Entry[] = reactive([]);
 
@@ -13,7 +14,9 @@ const user: User = reactive({
   username: "danielkelly_io",
   settings: [],
 });
-console.log(user.id);
+
+provide(userInjectionKey, user);
+
 const handleCreateEntry = (entry: Entry) => {
   entries.unshift(entry);
 };
